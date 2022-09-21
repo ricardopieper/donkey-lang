55e2a93c9fade267d19bf0c42c9dd97d3bc428ee
Ricardo Pieper <ricardopieper@live.com>
2022-09-20 21:30:22 -0300

    Removed garbage files

--- example.py
+++
@@ -1,5 +0,0 @@
-def my_function(arg1: i32, arg2: i32) -> i32:
-    if arg1 == arg2:
-        return 0
-    else:
-        return arg1 * arg2 / (arg2 - arg1)
--- typecheck.py
+++
@@ -1,4 +0,0 @@
-def main(args: array<str>):
-    y = args[1].as_i32()
-    x = 1 + 2 / y
-    print(x)
